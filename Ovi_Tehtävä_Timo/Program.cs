6f89cdce43a4a301411eff4b7c98ca335774d9c2
Pepedik <s2300864@edu.bc.fi>
2025-01-27 13:00:34 +0200

    Ovi ja ruokaannos tehtävä

---
+++ Ovi_Tehtävä_Timo/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Ovi_Tehtävä_Timo
+{
+    internal class Program
+    {
+        enum Oventila
+        {
+            Auki,
+            Kiinni,
+            Lukossa
+        }
+        enum Toiminto
+        {
+            Avaa = 0,
+            Lukitse = 1,
+            Avaa_Lukko = 2,
+            Sulje = 3,
+        }
+        static void Main(string[] args)
+        {
+            Oventila tila = Oventila.Auki;
+            string[] toiminnot = Enum.GetNames<Toiminto>();
+            Console.WriteLine($"Ovi on nyt {tila}");
+            while (true)
+            {
+
+
+                Console.WriteLine("Valitse toiminta.");
+                for (int i = 0; i < toiminnot.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {toiminnot[i]}");
+                }
+                string vastaus = Console.ReadLine();
+                Toiminto valittu = Toiminto.Avaa;
+                for (int i = 0; i < toiminnot.Length; i++)
+                {
+                    if (toiminnot[i].ToLower() == vastaus.ToLower())
+                    {
+                        valittu = (Toiminto)i;
+                        Console.WriteLine($"Valitsit {valittu}");
+                    }
+                }
+                if (valittu <= Toiminto.Avaa && Oventila.Kiinni == tila)
+                {
+                    tila = Oventila.Auki;
+                    Console.WriteLine($"Ovi on nyt {tila}");
+                }
+                else if (tila == Oventila.Kiinni && Toiminto.Avaa == valittu)
+                {
+                    tila = Oventila.Auki;
+                    Console.WriteLine($"Ovi on nyt {tila}");
+                }
+                else if (tila == Oventila.Auki && Toiminto.Sulje == valittu)
+                {
+                    tila = Oventila.Kiinni;
+                    Console.WriteLine($"Ovi on nyt {tila}");
+                }
+                else if (tila == Oventila.Kiinni && Toiminto.Lukitse == valittu)
+                {
+                    tila = Oventila.Lukossa;
+                    Console.WriteLine($"Ovi on nyt {tila}");
+                }
+                else if (tila == Oventila.Lukossa && Toiminto.Avaa_Lukko == valittu)
+                {
+                    tila = Oventila.Kiinni;
+                    Console.WriteLine($"Ovi on nyt {tila}");
+                }
+            }
+        }
+    }
+}
---
+++ Ruoka_Annos/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Ruoka_Annos
+{
+    internal class Program
+    {
+        enum PääRaakaAine
+        {
+            nautaa = 0, kanaa = 1, kasviksia = 2
+        }
+        enum Kastike
+        {
+            curry = 0, hapanimelä = 1, pippuri = 2, chili = 3
+        }
+        enum Lisuke
+        {
+            perunaa = 0, riisiä = 1, pastaa = 2
+        }
+
+        class Ateria
+        {
+            public override string ToString()
+            {
+                return $"Pääraaka-aine: {pääaine}, Lisuke: {lisuke}, Kastike: {kastike}";
+            }
+            public PääRaakaAine pääaine;
+            public Kastike kastike;
+            public Lisuke lisuke;
+
+            public Ateria(PääRaakaAine pääaine, Lisuke lisuke, Kastike kastike)
+            {
+                this.pääaine = pääaine;
+                this.lisuke = lisuke;
+                this.kastike = kastike;
+            }
+        }
+        static void Main(string[] args)
+        {
+            PääRaakaAine pAine1 = PääRaakaAine.kanaa;
+            Kastike kastike1 = Kastike.chili;
+            Lisuke lisuke1 = Lisuke.perunaa;
+
+            string[] praakaaine = Enum.GetNames<PääRaakaAine>();
+
+            for (int i = 0; i < praakaaine.Length; i++)
+            {
+                Console.WriteLine($"{praakaaine[i]} ");
+            }
+
+            Console.WriteLine("Valitse raakaianeet");
+            string vastaus = Console.ReadLine();
+            for (int i = 0; i < praakaaine.Length; i++)
+            {
+                if (praakaaine[i].ToLower() == vastaus.ToLower())
+                {
+                    pAine1 = (PääRaakaAine)i;
+                    Console.WriteLine($"Valitsit {pAine1}");
+                }
+            }
+            string[] Lisuke1 = Enum.GetNames<Lisuke>();
+
+            for (int i = 0; i < Lisuke1.Length; i++)
+            {
+                Console.WriteLine($"{Lisuke1[i]} ");
+            }
+
+            Console.WriteLine("Valitse Lisukkeet");
+            string vastaus1 = Console.ReadLine();
+            for (int i = 0; i < Lisuke1.Length; i++)
+            {
+                if (Lisuke1[i].ToLower() == vastaus1.ToLower())
+                {
+                    lisuke1 = (Lisuke)i;
+                    Console.WriteLine($"Valitsit {lisuke1}");
+                }
+            }
+            string[] Kastike1 = Enum.GetNames<Kastike>();
+
+            for (int i = 0; i < Kastike1.Length; i++)
+            {
+                Console.WriteLine($"{Kastike1[i]} ");
+            }
+
+            Console.WriteLine("Valitse Kastikkeet");
+            string vastaus2 = Console.ReadLine();
+            for (int i = 0; i < Kastike1.Length; i++)
+            {
+                if (Kastike1[i].ToLower() == vastaus2.ToLower())
+                {
+                    kastike1 = (Kastike)i;
+                    Console.WriteLine($"Valitsit {kastike1}");
+                }
+            }
+    
+            
+            Ateria Ruoka = new Ateria(pAine1, lisuke1, kastike1);
+            Console.WriteLine(Ruoka.ToString());
+
+
+        }
+    }
+}
